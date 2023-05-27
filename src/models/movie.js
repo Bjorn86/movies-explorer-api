146ebe6bdcebbf1edad48cabd321b81ec42dee1e
Danila Legkobytov <bjorn-bis@yandex.ru>
2023-05-27 21:46:26 +0700

    feat: Создать схемы и модели данных БД

---
+++ src/models/movie.js
@@ -0,0 +1,71 @@
+// IMPORT PACKAGES
+const mongoose = require('mongoose');
+const isURL = require('validator/lib/isURL');
+
+// MOVIE SCHEMA
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Должна быть указана страна создания фильма'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Должен быть указан режиссёр фильма'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Должна быть указана продолжительность фильма'],
+  },
+  year: {
+    type: String,
+    required: [true, 'Должен быть указан год выпуска фильма'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Должно быть указано описание фильма'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Должна быть указана ссылка на постер фильма'],
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Должна быть указана ссылка на трейлер фильма'],
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'Должна быть указана ссылка на мини-постер фильма'],
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, 'Должен быть указан владелец карточки фильма'],
+  },
+  movieId: {
+    type: Number,
+    required: [true, 'Должен быть указан ID фильма'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Должно быть указано название фильма на русском языке'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Должно быть указано название фильма на английском языке'],
+  },
+});
+
+// MODULE EXPORT
+module.exports = mongoose.model('movie', movieSchema);
---
+++ src/models/user.js
@@ -0,0 +1,54 @@
+// IMPORT PACKAGES
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+const bcrypt = require('bcryptjs');
+
+// IMPORT ERRORS
+const AuthorizationError = require('../errors/authorizationError');
+
+// USER SCHEMA
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "password" должно быть заполнено'],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" 2 символа'],
+    maxlength: [30, 'Максимальная длина поля "name" 30 символов'],
+  },
+}, {
+  versionKey: false,
+  // FIND USER BY CREDENTIALS METHOD
+  statics: {
+    findUserByCredentials(email, password) {
+      return this.findOne({ email }).select('+password')
+        .then((user) => {
+          if (!user) {
+            throw new AuthorizationError('Неправильная почта или пароль');
+          }
+          return bcrypt.compare(password, user.password)
+            .then((matched) => {
+              if (!matched) {
+                throw new AuthorizationError('Неправильная почта или пароль');
+              }
+              return user;
+            });
+        });
+    },
+  },
+});
+
+// MODULE EXPORT
+module.exports = mongoose.model('user', userSchema);
